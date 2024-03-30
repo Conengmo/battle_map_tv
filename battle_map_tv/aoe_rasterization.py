12ebdb419a4a2e18cf181ba9734b19e4b5588fdd
Frank <33519926+Conengmo@users.noreply.github.com>
2024-03-30 14:50:17 +0100

    follow one cone edge (WIP)

--- battle_map_tv/aoe_rasterization.py
+++ battle_map_tv/aoe_rasterization.py
@@ -80,7 +80,90 @@ class CircleEdges:
         return final_points
 
 
-if __name__ == "__main__":
+def cone_to_polygon(size: int, theta, grid):
+    delta = 1  # grid.pixels_per_inch_mean  # noqa
+
+    edges = []
+    start_point = (0, 0)
+    edges.append(start_point)
+    x_prev, y_prev = start_point
+
+    # angle between center line and edge
+    phi = math.atan(0.5)
+    # angle between x-axis and top/bottom edge
+    gamma_t = theta + phi
+    gamma_b = theta - phi  # noqa
+    # angle between y-axis and far edge
+    gamma_z = math.radians(180) - phi + gamma_t
+
+    # angle between center line and edge
+    phi = math.atan(0.5)
+    # size of top edge
+    a_t = size / math.cos(phi)
+    # coordinate of top point
+    x_t_final = a_t * math.cos(gamma_t)
+    y_t_final = a_t * math.sin(gamma_t)
+    edges, x_prev, y_prev = thing(
+        gamma=gamma_t,
+        gamma_next=gamma_z,
+        edges=edges,
+        p_prev=(x_prev, y_prev),
+        p_final=(x_t_final, y_t_final),
+    )
+
+    return edges
+
+
+def thing(gamma, gamma_next, edges, p_prev, p_final):
+    delta = 1
+    x_prev, y_prev = p_prev
+    x_t_final, y_t_final = p_final
+
+    while True:
+        x = x_prev + delta
+
+        y_t_prev = x_prev * math.tan(gamma)
+        y_t = min(y_t_final, x * math.tan(gamma))
+
+        area = area_under_line(x_prev=x_prev, x=x, y_prev=y_t_prev, y=y_t, y_rast_prev=y_prev)
+
+        if x > x_t_final:
+            # 'go around the corner' on to the next edge
+            x_star_next = x_t_final + (y_t_final - y_prev) * math.tan(gamma_next)
+            _area = area_under_line(
+                x_prev=y_t,
+                x=y_prev,
+                y_prev=x_t_final,
+                y=x_star_next,
+                y_rast_prev=x_t_final,
+            )
+            area += _area
+
+        if area >= 0.5 * delta**2:
+            y = y_prev + delta
+            edges.append((x_prev, y))
+        elif x > x_t_final:
+            break
+        else:
+            y = y_prev
+
+        edges.append((x, y))
+        x_prev = x
+        y_prev = y
+
+        if x > x_t_final:
+            break
+
+    return edges, x_prev, y_prev
+
+
+def area_under_line(x_prev, x, y_prev, y, y_rast_prev) -> float:
+    dx = abs(x - x_prev)
+    area = dx * y_prev + 0.5 * dx * (y - y_prev) - dx * y_rast_prev
+    return area
+
+
+def main():
     import matplotlib.pyplot as plt
     import numpy as np
 
@@ -88,7 +171,7 @@ if __name__ == "__main__":
     size = 4
     a = size
     # angle between x-axis and center line
-    theta = math.radians(10)
+    theta = math.radians(0)
     # angle between center line and edge
     phi = math.atan(0.5)
     # angle between x-axis and top edge
@@ -116,8 +199,15 @@ if __name__ == "__main__":
 
     print("size top to bottom:", math.sqrt((y_t - y_b) ** 2 + (x_t - x_b) ** 2))
 
+    edges = cone_to_polygon(size, theta, grid=None)
+    ax.plot(*zip(*edges), "y-")
+
     ax.xaxis.set_ticks(np.arange(0, size + 1, 1.0))
     ax.yaxis.set_ticks(np.arange(-size / 2, size / 2 + 1, 1.0))
     ax.grid(True, alpha=0.3)
     plt.axis("equal")
     plt.show()
+
+
+if __name__ == "__main__":
+    main()
