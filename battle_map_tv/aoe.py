099f3f18a4b9eaa05d2463785f7e7231d0cc3efc
Frank <33519926+Conengmo@users.noreply.github.com>
2024-04-01 18:08:37 +0200

    allow square rotation

--- battle_map_tv/aoe.py
+++ battle_map_tv/aoe.py
@@ -245,15 +245,28 @@ class Square(BaseShape):
         scene: QGraphicsScene,
         size: Optional[float] = None,
     ):
-        x2, y2 = self._fix_aspect_ratio(x1=x1, y1=y1, x2=x2, y2=y2, grid=grid, size=size)
-        left = min(x1, x2)
-        top = min(y1, y2)
-        right = max(x1, x2)
-        bottom = max(y1, y2)
-        width = right - left
-        height = bottom - top
-        self.shape = QGraphicsRectItem(left, top, width, height)
-        self.size = width
+        if size is not None:
+            self.size = size
+        else:
+            self.size = self._calculate_size(x1=x1, y1=y1, x2=x2, y2=y2, grid=None) / math.sqrt(2)
+            if grid is not None:
+                self.size = grid.normalize_size(size=self.size)
+        angle = self._get_angle_radians(x1=x1, y1=y1, x2=x2, y2=y2, grid=grid)
+        point_2 = (
+            x1 + self.size * math.sqrt(2) * math.cos(angle),
+            y1 + self.size * math.sqrt(2) * math.sin(angle),
+        )
+        point_1 = (
+            x1 + self.size * math.cos(angle + math.pi / 4),
+            y1 + self.size * math.sin(angle + math.pi / 4),
+        )
+        point_3 = (
+            x1 + self.size * math.cos(angle - math.pi / 4),
+            y1 + self.size * math.sin(angle - math.pi / 4),
+        )
+        self.shape = QGraphicsPolygonItem(
+            QPolygonF.fromList([QPointF(*p) for p in [(x1, y1), point_1, point_2, point_3]])
+        )
         super().__init__(scene=scene)
 
     @staticmethod
